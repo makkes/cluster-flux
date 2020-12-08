db787eeedd6c7979be5db98650b9be5676e259ea
Max Jonas Werner <mail@makk.es>
2020-12-08 19:31:03 +0100

    enable different environments

--- up.sh
+++ up.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
+ENV=${1:-stable}
+KUSTOMIZATION="${ENV}-kustomization.yaml"
+[ -f ${KUSTOMIZATION} ] || (echo "unknown environment '${ENV}'" && exit 1)
+
 kind create cluster --config=kind-config.yaml
 
 function seed_images() {
@@ -13,4 +19,4 @@ seed_images
 
 flux install
 
-kubectl apply -f source.yaml -f stable-kustomization.yaml
+kubectl apply -f source.yaml -f "${KUSTOMIZATION}"
