6c1a2fd62e3cc08fcb1ef1d2efb3bbeba5772c6e
Max Jonas Werner <mwerner@d2iq.com>
2021-02-22 16:29:03 +0100

    feat: update Flux; add konvoy environment; get rid of images
    Maintaining the images list is too cumbersome so I removed that
    feature as it's more of a nice to have than a real requirement.

--- up.sh
+++ up.sh
@@ -6,16 +6,9 @@ ENV=${1:-stable}
 KUSTOMIZATION="${ENV}-kustomization.yaml"
 [ -f ${KUSTOMIZATION} ] || (echo "unknown environment '${ENV}'" && exit 1)
 
-kind create cluster --config=kind-config.yaml
-
-function seed_images() {
-    for image in $(cat images.txt) ; do
-        [[ "$(docker images -q ${image})" == "" ]] && docker pull ${image}
-        kind load docker-image ${image}
-    done
-}
-
-seed_images
+if [[ "${2:-}" == "kind" ]] ; then
+    kind create cluster --config=kind-config.yaml
+fi
 
 kubectl apply -f flux.yaml
 kubectl apply -f source.yaml -f "${KUSTOMIZATION}"
